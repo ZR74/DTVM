edb76585cd54d85233ca0b1d9de10d80addbcde0
root <2401889661@qq.com>
2025-08-16 09:16:00 +0000

    feat: add examples for Solidity contract interactions

---
+++ tests/evm_solidity/caller/caller.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../callee/callee.sol";
+
+contract CallerContract {
+    CalleeContract public callee;
+
+    constructor(address calleeAddress) {
+        callee = CalleeContract(calleeAddress);
+    }
+
+    function setCalleeX(uint256 _x) external {
+        callee.setX(_x);
+    }
+
+    function callAdd(uint256 a, uint256 b) external view returns (uint256) {
+        return callee.add(a, b);
+    }
+
+    function callIncrementX() external {
+        callee.incrementX();
+    }
+
+    function getCalleeX() external view returns (uint256) {
+        return callee.getX();
+    }
+}
